3c5033f5b9e50157275ee0830966e28ade967b43
ArayaPablo <132422611+Araya03@users.noreply.github.com>
2026-09-24 21:52:44 -0600

    Mejoras en el ejercicio 4.

--- Ejercicio 4/Ejercicio 4.s
+++ Ejercicio 4/Ejercicio 4.s
@@ -1,29 +1,30 @@
-.data
-N: .word 10 #Valor de N almacenado en memoria (ej. N = 10)
-result: .word 0 #Espacio para almacenar el resultado final
-
-.text
-
 .global _start
 _start:
 	
-		#Cargar N desde la memoria en el registro t1
-        la t0, N
-        lw t1, 0(t0) #t1 = N
-        
-        #Inicializar variables del bucle
-        li t2, 0 #t2(i) = 0 (contador)
-        li t3, 0 #t3(suma) = 0 (acumulador)
+.global _start
+_start:
+	
+addi t0, zero, 5 #Valor de N
+sw t0, 0(zero) #Cargo N en memoria
 
-.while:
-        #Condición: while (i < N) -> si i >= N, salir del bucle
-        bge t2, t1, .fin
-        
-        addi t2, t2, 1 #i = i + 1
-        add t3, t3, t2 #suma = suma + i
-        j .while #Volver al inicio del bucle
+lw t0, 0(zero) #Cargo N desde la memoria
 
-.fin:
-        #Guardar el resultado en la variable 'result' en memoria
-        la t0, result
-        sw t3, 0(t0)
+addi t1, zero, 0 #Suma
+addi t2, zero, 1 #i
+
+analiza:
+
+#Si i <= N sigue en el while
+ble t2, t0, continua_while
+
+#Si i > N sale del while
+bgt t2, t0, fin_while
+
+
+continua_while:
+add t1, t1, t2
+addi t2, t2, 1
+j analiza
+
+fin_while:
+sw t1, 4(zero)
